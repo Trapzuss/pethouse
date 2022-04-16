422bbb25691212175ef3ffe671061ff138f7acf3
Pepo-Pathompong <70483000+Pepo-Pathompong@users.noreply.github.com>
2022-04-16 20:25:53 +0700

    [ADD] Register

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,12 +1,63 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
+
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({ Key? key }) : super(key: key);
+  RegisterScreen({Key? key}) : super(key: key);
+
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Register"),),
+      appBar: AppBar(
+        title: Text("Register"),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Form(
+              key: formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Email",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      "Password",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          child: Text(
+                            "Register",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )),
+        ),
+      ),
     );
   }
 }
