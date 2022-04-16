8ede59106babe8b4df28735c794457ba74c010b6
Pepo-Pathompong <70483000+Pepo-Pathompong@users.noreply.github.com>
2022-04-16 13:25:00 +0700

    UPDATE [Home Page]

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,60 +7,70 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Pet's house"),
-      ),
+      // appBar: AppBar(
+      //   title: Text("Pet's house"),
+      // ),
       body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomRight,
+                colors: [
+              Color(0xfffed269),
+              Color.fromARGB(255, 241, 200, 50)
+            ])),
         child: Padding(
           padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.asset("assets/images/logo.png"),
-                Text("Pets's House",
-                    style: GoogleFonts.freckleFace(
-                        fontSize: 60, color: Colors.brown[800])),
-                Container(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text("Beautiful community with on our hands!."),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton.icon(
-                    icon: Icon(Icons.add),
-                    label: Text("Register", style: TextStyle(fontSize: 20)),
-                    onPressed: () {},
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/logo.png"),
+                  Text("Pets's House",
+                      style: GoogleFonts.freckleFace(
+                          fontSize: 60, color: Colors.brown[800])),
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Beautiful community with on our hands!."),
                   ),
-                ),
-                Padding(padding: const EdgeInsets.all(8.0)),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton.icon(
-                    icon: Icon(Icons.login),
-                    label: Text("Login", style: TextStyle(fontSize: 20)),
-                    onPressed: () {},
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
                   ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton.icon(
+                      icon: Icon(Icons.add),
+                      label: Text("Register", style: TextStyle(fontSize: 20)),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(padding: const EdgeInsets.all(8.0)),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton.icon(
+                      icon: Icon(Icons.login),
+                      label: Text("Login", style: TextStyle(fontSize: 20)),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
                         mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Image.asset(
-                          "assets/images/footcat.png",
-                          width: 200,
-                          height: 200,
-                          fit: BoxFit.contain,
-                        ),
-                      ],
-                    )
-                  ],
-                )
-              ],
+                        children: [
+                          Image.asset(
+                            "assets/images/footcat.png",
+                            width: 300,
+                            height: 500,
+                            fit: BoxFit.contain,
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
@@ -68,4 +78,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
