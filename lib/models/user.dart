6233c4bf4210b1a4b416201594896071370a9202
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-05-05 05:26:59 +0700

    ADD mock userData

---
+++ lib/models/user.dart
@@ -0,0 +1,12 @@
+class User {
+  final String imagePath;
+  final String username;
+  final String bio;
+  final String email;
+
+  const User(
+      {required this.imagePath,
+      required this.username,
+      required this.bio,
+      required this.email});
+}
