31b2676a58dc96516344ce241a7576f961f04642
Pepo-Pathompong <70483000+Pepo-Pathompong@users.noreply.github.com>
2022-05-05 00:49:24 +0700

    [ADD] Register

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -2,6 +2,8 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pet_house/screens/login.dart';
 
 class RegisterScreen extends StatelessWidget {
   RegisterScreen({Key? key}) : super(key: key);
@@ -11,84 +13,174 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: firebase,
-      builder: (context, snapshot){
-        if(snapshot.hasError){
-          return Scaffold(
-            appBar: AppBar(title: Text("Error"),
-          ),
-          body: Center(child: Text("${snapshot.error}"),
-          ),
-          );
-        }
-        if(snapshot.connectionState == ConnectionState.done){
-          return Scaffold(
-      appBar: AppBar(
-        title: Text("Register"),
-      ),
+    return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomRight,
+                colors: [
+              Color.fromARGB(255, 255, 254, 254),
+              Color.fromARGB(255, 247, 215, 127),
+            ])),
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Form(
-              key: formKey,
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Username",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    TextFormField(
-                      
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      "Email",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      "Password",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          child: Text(
-                            "Register",
-                            style: TextStyle(fontSize: 20),
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/pets.png",width: 200,),
+                  Text("Pets's House",
+                      style: GoogleFonts.freckleFace(
+                          fontSize: 40, color: Colors.brown[800])),
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Beautiful community with on our hands!."),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Username",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
                           ),
-                          onPressed: () {},
                         ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.person),
+                          hintText: 'username',
+                        ),
+                        keyboardType: TextInputType.name,
                       ),
-                    )
-                  ],
-                ),
-              )),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Email",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.email),
+                          hintText: 'email',
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                    ),
+                  ),
+                 Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Password",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.lock),
+                          // suffixIcon: Icon(Icons.visibility),
+                          hintText: 'password',
+                        ),
+                         keyboardType: TextInputType.number,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('SIGN UP'),
+                        ),
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Already have an Account?  ",
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return LoginScreen();
+                          }));
+                        },
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
-        }
-        return Scaffold(
-          body: Center(
-            child: CircularProgressIndicator.adaptive(),
-          ),
-          );
-      });
-    
+    //   future: firebase,
+    //   builder: (context, snapshot){
+    //     if(snapshot.hasError){
+    //       return Scaffold(
+    //         appBar: AppBar(title: Text("Error"),
+    //       ),
+    //       body: Center(child: Text("${snapshot.error}"),
+    //       ),
+    //       );
+    //     }
+    //     if(snapshot.connectionState == ConnectionState.done){
+    //       return Scaffold(
+
+    // );
+    //     }
+    //     return Scaffold(
+    //       body: Center(
+    //         child: CircularProgressIndicator.adaptive(),
+    //       ),
+    //       );
+    // }
   }
 }
