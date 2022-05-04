9fe4343696718f203ed3d0badb8bab7984cf6a56
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-05-04 19:08:39 +0700

    Update post widget

--- lib/widget/post/postWidget.dart
+++ lib/widget/post/postWidget.dart
@@ -3,14 +3,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Post extends StatefulWidget {
-  const Post({Key? key}) : super(key: key);
+class PostWidget extends StatefulWidget {
+  const PostWidget({Key? key}) : super(key: key);
 
   @override
-  State<Post> createState() => _PostState();
+  State<PostWidget> createState() => _PostWidgetState();
 }
 
-class _PostState extends State<Post> {
+class _PostWidgetState extends State<PostWidget> {
   final List<IconData> tags = [Icons.pets];
 
   @override
@@ -19,94 +19,7 @@ class _PostState extends State<Post> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                  child: Row(
-                children: [
-                  Container(
-                    width: 50,
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://image.joox.com/JOOXcover/0/fe58ed87bcd7937b/300'),
-                    ),
-                  ),
-                  Text(
-                    'Username',
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )
-                ],
-              )),
-              Container(
-                  child: Row(
-                children: [
-                  Container(
-                    width: 50,
-                    child: Icon(Icons.language),
-                  ),
-                  Container(
-                      width: 50,
-                      child: Icon(
-                        Icons.more_vert,
-                      ))
-                ],
-              ))
-            ],
-          ),
-          Container(
-            child: Image.network(
-                'https://image.joox.com/JOOXcover/0/fe58ed87bcd7937b/300'),
-          ),
-          Container(
-              padding: EdgeInsets.only(top: 8),
-              child: Center(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Flexible(
-                          child: Container(
-                        padding: EdgeInsets.only(top: 4),
-                        child: Text(
-                            'Actually he always hong bonkai everytime that i seen, Can anyone get them to pets pls!! ',
-                            style: TextStyle(fontSize: 12)),
-                      )),
-                      Column(
-                        children: [
-                          Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Icon(Icons.message),
-                                  Icon(Icons.favorite)
-                                ],
-                              )
-                            ],
-                          ),
-                          Row(
-                            children: [Text('1.4k Liked')],
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 4),
-                    child: Text(
-                      'View all 2 comments',
-                      style: TextStyle(
-                        color: Colors.black38,
-                      ),
-                    ),
-                  )
-                ],
-              )))
-        ],
+        children: [],
       ),
     );
   }
