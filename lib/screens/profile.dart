7a452a692c4a899889e3d6cd50207f4ec887a09d
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-05-05 05:27:40 +0700

    UPDATE profile page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,8 +1,15 @@
-// import 'dart:html';
-
 import 'dart:math';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:pet_house/screens/posts/post.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:pet_house/utils/user_preferences.dart';
+import 'dart:io';
+
+import 'package:pet_house/utils/utils.dart';
+import 'package:pet_house/widget/common/ImageWidget.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
@@ -12,67 +19,175 @@ class Profile extends StatefulWidget {
 }
 
 Column _buildButtonColumn(String label, String text) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+              fontSize: 10, color: AppTheme.colors.secondaryFontColor),
         ),
-        Text(
-            text,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-            ),
-          ),
-      ],
-    );
-  }
-  
+      ),
+      Text(
+        text,
+        style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: AppTheme.colors.primaryFontColor),
+      ),
+    ],
+  );
+}
 
 class _ProfileState extends State<Profile> {
+  final user = UserPreferences.mockUser;
+  List img = [
+    'https://www.familyeducation.com/sites/default/files/fe_slideshow/2008_03/Chipmunk_H.jpg',
+    'https://ae01.alicdn.com/kf/H35ab780c7bd04d81a9dae76bb729b9813/Vintage-Body-Deer-Cat-Dog-Portrait.jpg_640x640.jpg',
+    'https://www.familyeducation.com/sites/default/files/collection-item/InsectSpider_H.jpg',
+  ];
+  File? image;
+  Future pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+      final imageTemporary = File(image.path);
+      setState(() {
+        // print(imageTemporary);
+        this.image = imageTemporary;
+      });
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.all(0),
         child: Center(
-          child: Column(
-            children: [
-              Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-0xqWrYOTxA6pEN2dcsMhqHTAqFFjTr3UyA&usqp=CAU'),
-              Text('ta'),
-              Text('love cat',style: TextStyle(fontSize: 20),),
-              TextButton(onPressed: (){}, child: Text('text button')),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: Column(
+          children: [
+            image != null
+                ? ImageWidget(
+                    path: image!,
+                    onClicked: () async {
+                      pickImage(ImageSource.gallery);
+                    })
+                : ImageWidget(
+                    path: user.imagePath,
+                    onClicked: () async {
+                      pickImage(ImageSource.gallery);
+                    }),
+            Container(
+              padding: EdgeInsets.all(8),
+              child: Column(
                 children: [
-                  _buildButtonColumn('post', '20'),
-                  _buildButtonColumn('likes', '50'),
+                  Text(
+                    user.username,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    user.bio,
+                    style: TextStyle(color: AppTheme.colors.secondaryFontColor),
+                  )
                 ],
               ),
-              Container(
-                width: double.infinity,
-                decoration: BoxDecoration(color: Colors.green),
-                child: Column(
-                  children: [
-                    Text('My post'),
-                    Icon(Icons.newspaper)
-                  ]),
-              )
-            ],
-          )
-        
-        ),
+            ),
+            ElevatedButton(
+                onPressed: () async {},
+                child: Text('Edit'),
+                style: ElevatedButton.styleFrom(
+                    primary: AppTheme.colors.primaryFontColor,
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30)))),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildButtonColumn('Post', '20'),
+                _buildButtonColumn('Likes', '50'),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.all(4),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      color: AppTheme.colors.primaryFontColor.withOpacity(0.05),
+                      spreadRadius: 2,
+                      blurRadius: 5),
+                  BoxShadow(
+                      color: AppTheme.colors.primary.withOpacity(1),
+                      spreadRadius: 2,
+                      blurRadius: 0,
+                      offset: Offset(0, 4))
+                ],
+                // border: Border(bottom: BorderSide(width: 2)),
+                borderRadius: BorderRadius.vertical(top: Radius.circular(30.0)),
+              ),
+              child: Column(mainAxisSize: MainAxisSize.max, children: [
+                Text(
+                  'My post',
+                  style: TextStyle(
+                      color: AppTheme.colors.secondaryFontColor, fontSize: 12),
+                ),
+                Container(
+                  padding: EdgeInsets.all(4),
+                  child: Icon(
+                    Icons.newspaper,
+                    color: AppTheme.colors.primaryFontColor,
+                  ),
+                ),
+              ]),
+            ),
+            UserPicture()
+          ],
+        )),
       ),
     );
+  }
+
+  Widget UserPicture() {
+    return SafeArea(
+        child: Container(
+      margin: EdgeInsets.only(top: 12),
+      height: 180,
+      child: MasonryGridView.count(
+        crossAxisCount: 2,
+        itemCount: img.length,
+        mainAxisSpacing: 12,
+        crossAxisSpacing: 12,
+        // shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return InkWell(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(
+                  img[index],
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return Post();
+              }));
+            },
+          );
+        },
+      ),
+    ));
   }
 }
---
+++ lib/widget/common/ImageWidget.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+import 'package:pet_house/utils/utils.dart';
+
+class ImageWidget extends StatelessWidget {
+  final path;
+  final VoidCallback onClicked;
+
+  const ImageWidget({Key? key, required this.path, required this.onClicked})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        buildImage(),
+        Positioned(
+          child: buildEditIcon(AppTheme.colors.primary),
+          bottom: 0,
+          right: 4,
+        )
+      ],
+    );
+  }
+
+  Widget buildImage() {
+    // print(path);
+    // print(path.runtimeType);
+    // print(path.runtimeType == String);
+
+    if (path.runtimeType == String) {
+      final image = NetworkImage(path);
+      return ClipOval(
+        child: Material(
+          color: Colors.transparent,
+          child: Ink.image(
+            image: image,
+            fit: BoxFit.cover,
+            width: 128,
+            height: 128,
+            child: InkWell(onTap: onClicked),
+          ),
+        ),
+      );
+    } else {
+      return ClipOval(
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            child: Image.file(
+              path!,
+              height: 128,
+              fit: BoxFit.cover,
+            ),
+            onTap: onClicked,
+          ),
+        ),
+      );
+    }
+  }
+
+  Widget buildEditIcon(Color color) => CircleAvatar(
+        backgroundColor: AppTheme.colors.primaryFontColor,
+        child: Icon(
+          Icons.edit,
+          size: 20,
+          color: Colors.white,
+        ),
+      );
+
+  Future<ImageSource?> showImageSource(BuildContext context) async {
+    if (Platform.isIOS) {
+      return showCupertinoModalPopup<ImageSource>(
+          context: context,
+          builder: (context) => CupertinoActionSheet(
+                actions: [
+                  CupertinoActionSheetAction(
+                      onPressed: () =>
+                          Navigator.of(context).pop(ImageSource.camera),
+                      child: Text('Camera')),
+                  CupertinoActionSheetAction(
+                      onPressed: () =>
+                          Navigator.of(context).pop(ImageSource.gallery),
+                      child: Text('Gallery'))
+                ],
+              ));
+    } else {
+      return showModalBottomSheet(
+          context: context,
+          builder: (context) => Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.camera_alt),
+                    title: Text('Camera'),
+                    onTap: () => Navigator.of(context).pop(ImageSource.camera),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.image),
+                    title: Text('Gallery'),
+                    onTap: () => Navigator.of(context).pop(ImageSource.gallery),
+                  )
+                ],
+              ));
+    }
+  }
+}
