ca96bf9c1ad721d6d3e53f406e3ffccb7f25998f
Pepo-Pathompong <70483000+Pepo-Pathompong@users.noreply.github.com>
2022-05-05 00:49:02 +0700

    [ADD] Forgot

---
+++ lib/screens/forgot.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pet_house/screens/login.dart';
+
+class Forgot extends StatelessWidget {
+  const Forgot({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomRight,
+                colors: [
+              Color.fromARGB(255, 255, 254, 254),
+              Color.fromARGB(255, 247, 215, 127),
+            ])),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/pets.png",width: 200,),
+                  Text("Pets's House",
+                      style: GoogleFonts.freckleFace(
+                          fontSize: 40, color: Colors.brown[800])),
+                  Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Beautiful community with on our hands!."),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 20, 0, 0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Email",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.email),
+                          hintText: 'email',
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                      width: double.infinity,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 16.0),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text('SUBMIT'),
+                        ),
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return LoginScreen();
+                          }));
+                        },
+                        child: Text(
+                          "Back to Login",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
