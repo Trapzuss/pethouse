c6d9563583eb156e57af306d9ec17c7516c5699a
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-05-08 04:43:46 +0700

    SETUP firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pet_house/layout/default_layout.dart';
 import 'package:pet_house/screens/home.dart';
 import 'package:pet_house/utils/utils.dart';
 
-void main() {
+// void main() {
+//   runApp(const MyApp());
+// }
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
