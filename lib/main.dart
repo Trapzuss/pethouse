024d36dc5227b7227c9d06b304b498c91e900427
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-05-10 04:33:48 +0700

    ADD authWrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:pet_house/screens/edit_Profile.dart';
+import 'package:pet_house/screens/edit_profile.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:pet_house/screens/feed.dart';
+import 'package:pet_house/screens/login.dart';
+import 'package:pet_house/services/authentication_services.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -20,14 +26,15 @@ Future main() async {
   runApp(const MyApp());
 }
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       theme: ThemeData(
           scaffoldBackgroundColor: Color.fromARGB(240, 255, 255, 255),
           textTheme: GoogleFonts.interTextTheme(
@@ -39,7 +46,7 @@ class MyApp extends StatelessWidget {
               .copyWith(primary: AppTheme.colors.primary),
           appBarTheme: AppBarTheme(foregroundColor: Color(0xFF263238))),
       title: 'Flutter Demo',
-      home: DefaultLayout(),
+      home: AuthenticationWrapper(),
       navigatorObservers: [BotToastNavigatorObserver()],
       builder: BotToastInit(),
 
@@ -55,6 +62,22 @@ class AuthenticationWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+              child: SpinKitDancingSquare(
+            color: AppTheme.colors.primary,
+          ));
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Something went wrong!'));
+        } else if (snapshot.hasData) {
+          return DefaultLayout();
+        } else {
+          return LoginScreen();
+        }
+      },
+    );
   }
 }
