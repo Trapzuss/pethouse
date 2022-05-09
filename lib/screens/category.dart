814bd772aaa85c321e339fb7530246426b05f34a
Pepo-Pathompong <70483000+Pepo-Pathompong@users.noreply.github.com>
2022-05-09 23:00:12 +0700

    [ADD] Category Page

---
+++ lib/screens/category.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+import 'package:pet_house/screens/feed.dart';
+
+class Category extends StatefulWidget {
+  const Category({Key? key}) : super(key: key);
+
+  @override
+  State<Category> createState() => _CategoryState();
+}
+
+class _CategoryState extends State<Category>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xfffed269),
+        elevation: 0.0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return Feed();
+            }));
+          },
+        ),
+        title: Text(
+          "Choose you animal classes",
+          style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height - 30.0,
+        width: double.infinity,
+        child: TabBarView(controller: _tabController, children: [
+          ButtomPage(),
+          ButtomPage(),
+          ButtomPage(),
+        ]),
+      ),
+    );
+  }
+}
+
+class ButtomPage extends StatefulWidget {
+  const ButtomPage({Key? key}) : super(key: key);
+
+  @override
+  State<ButtomPage> createState() => _ButtomPageState();
+}
+
+class _ButtomPageState extends State<ButtomPage> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfffcfaf8),
+      body: ListView(
+        children: <Widget>[
+          SizedBox(
+            height: 15.0,
+          ),
+          Center(
+            child: Container(
+              padding: EdgeInsets.only(right: 15.0),
+              width: MediaQuery.of(context).size.width - 30.0,
+              height: MediaQuery.of(context).size.height - 50.0,
+              child: GridView.count(
+                crossAxisCount: 2,
+                primary: false,
+                crossAxisSpacing: 10.0,
+                mainAxisSpacing: 15.0,
+                childAspectRatio: 0.8,
+                children: <Widget>[
+                  _buildCard('assets/images/fish.png', 'Fishs'),
+                  _buildCard('assets/images/amphibians.png', 'Amphibians'),
+                  _buildCard('assets/images/aves.png', 'Aves'),
+                  _buildCard('assets/images/mammals.png', 'Mammals'),
+                  _buildCard('assets/images/reptiles.png', 'Reptiles'),
+                  _buildCard('assets/images/insecta.png', 'Insecta'),
+                  _buildCard('assets/images/cephalopod.png', 'Cephalopod'),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 15.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCard(String imgPath, String name) {
+
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.black;
+      }
+      return Colors.grey;
+    }
+    
+    return Padding(
+      padding: EdgeInsets.only(top: 5.0, bottom: 5.0, left: 5.0, right: 5.0),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  spreadRadius: 3.0,
+                  blurRadius: 5.0,
+                )
+              ],
+              color: Color(0xfffed269)),
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.all(5.0)),
+              Hero(
+                  tag: imgPath,
+                  child: Container(
+                    height: 75.0,
+                    width: 75.0,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage(imgPath), fit: BoxFit.contain)),
+                  )),
+              SizedBox(
+                height: 7.0,
+              ),
+              Text(
+                name,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Container(
+                  color: Color(0xffebebeb),
+                  height: 1.0,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 5.0, right: 5.0, top: 5.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25.0)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                         Checkbox(
+                                    checkColor: Colors.white,
+                                    fillColor: MaterialStateProperty.resolveWith(getColor),
+                                    value: isChecked,
+                                    onChanged: (bool? value) {
+                                      setState(() {
+                                        isChecked = value!;
+                                      });
+                                    }, 
+                                  ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Total',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
