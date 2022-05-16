a2a81bbd74df9f7e9f02b762d1af468b000469c4
Trapzuss <nonthakorn.inthong@mail.kmutt.ac.th>
2022-05-16 22:54:03 +0700

    [UPDATE] collections, collection - screen

--- lib/screens/collections/collection.dart
+++ lib/screens/collections/collection.dart
@@ -1,16 +1,17 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:pet_house/models/collection.dart';
+import 'package:pet_house/models/post.dart';
 import 'package:pet_house/screens/posts/post.dart';
+import 'package:pet_house/services/collection_services.dart';
+import 'package:pet_house/widget/common/emptyWidget.dart';
 import 'package:pet_house/widget/post/postWidget.dart';
 
-class collectionScreen extends StatefulWidget {
-  const collectionScreen({Key? key}) : super(key: key);
-
-  @override
-  State<collectionScreen> createState() => _collectionScreenState();
-}
-
-class _collectionScreenState extends State<collectionScreen> {
+class collectionScreen extends StatelessWidget {
+  final List<dynamic> postsId;
+  collectionScreen({Key? key, required this.postsId}) : super(key: key);
   List img = [
     'https://www.familyeducation.com/sites/default/files/fe_slideshow/2008_03/Chipmunk_H.jpg',
     'https://www.familyeducation.com/sites/default/files/collection-item/PotbellyPig_H.jpg',
@@ -25,41 +26,49 @@ class _collectionScreenState extends State<collectionScreen> {
         title: Text('Collection'),
       ),
       body: SafeArea(
-          child: Container(
-              child: MasonryGridView.count(
-        crossAxisCount: 2,
-        itemCount: img.length,
-        mainAxisSpacing: 12,
-        crossAxisSpacing: 12,
-        // shrinkWrap: true,
-        itemBuilder: (context, index) {
-          return InkWell(
-            // child: Container(
-            //   decoration: BoxDecoration(
-            //     borderRadius: BorderRadius.circular(8.0),
-            //   ),
-            //   child: ClipRRect(
-            //     borderRadius: BorderRadius.circular(8.0),
-            //     child: Image.network(
-            //       img[index],
-            //       fit: BoxFit.fill,
-            //     ),
-            //   ),
-            // ),
-            child: postWidget(
-              path: img[index],
-            ),
-            onTap: () {
-              // TODO getCollections and send id as prop to PostScreen
-              // Navigator.push(context, MaterialPageRoute(builder: (context) {
-              //   return PostScreen();
-              // }
-              // ));
-              print('go to post in collections');
-            },
-          );
+          child: FutureBuilder<List<PostModel?>>(
+        future: CollectionServices().getPostsInCollection(postsId),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return EmptyPostsTypeError(error: snapshot.error.toString());
+          } else if (snapshot.hasData) {
+            final posts = snapshot.data;
+            // log('$posts');
+            if (posts!.isEmpty) {
+              return EmptyPostsTypeEmpty();
+            }
+
+            return Container(
+                margin: EdgeInsets.all(8),
+                child: MasonryGridView.count(
+                  crossAxisCount: 2,
+                  itemCount: posts.length,
+                  mainAxisSpacing: 12,
+                  crossAxisSpacing: 12,
+                  // shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      child: postWidget(
+                          path: posts[index]!.imageUrl,
+                          postId: posts[index]!.postId),
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return PostScreen(id: posts[index]!.postId);
+                        }));
+                        // print('go to post in collections');
+                      },
+                    );
+                  },
+                ));
+          } else {
+            return Center(
+                child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ));
+          }
         },
-      ))),
+      )),
     );
   }
 }
--- lib/screens/collections/collections.dart
+++ lib/screens/collections/collections.dart
@@ -1,12 +1,18 @@
 import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:pet_house/models/collection.dart';
+import 'package:pet_house/models/post.dart';
 import 'package:pet_house/screens/collections/collection.dart';
 import 'package:pet_house/screens/posts/post.dart';
+import 'package:pet_house/services/collection_services.dart';
+import 'package:pet_house/services/post_services.dart';
 import 'package:pet_house/utils/utils.dart';
-import 'package:pet_house/widget/discovery/collectionchip.dart';
+import 'package:pet_house/widget/collection/collectionchip.dart';
+import 'package:pet_house/widget/common/emptyWidget.dart';
+import 'dart:developer';
 
 class collectionsScreen extends StatefulWidget {
   const collectionsScreen({Key? key}) : super(key: key);
@@ -16,26 +22,6 @@ class collectionsScreen extends StatefulWidget {
 }
 
 class _collectionsScreenState extends State<collectionsScreen> {
-  // var userCollections = [
-  //   {
-  //     'images': [
-  //       'https://www.familyeducation.com/sites/default/files/fe_slideshow/2008_03/Chipmunk_H.jpg',
-  //       'https://media1.popsugar-assets.com/files/thumbor/PZXP_YZYLIecUhZhKVpH0ThoAsM/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/18/646/n/1922243/3a21fb761112a072_pet/i/Royal-Pet-Portraits.jpg',
-  //       'https://www.familyeducation.com/sites/default/files/collection-item/Cockatiels_H.jpg',
-  //       'https://ae01.alicdn.com/kf/H35ab780c7bd04d81a9dae76bb729b9813/Vintage-Body-Deer-Cat-Dog-Portrait.jpg_640x640.jpg'
-  //     ],
-  //     'name': 'Interested stuff',
-  //     'amount': 4
-  //   },
-  //   {
-  //     'images': [
-  //       'https://www.familyeducation.com/sites/default/files/collection-item/Cockatiels_H.jpg',
-  //       'https://ae01.alicdn.com/kf/H35ab780c7bd04d81a9dae76bb729b9813/Vintage-Body-Deer-Cat-Dog-Portrait.jpg_640x640.jpg'
-  //     ],
-  //     'name': 'Cutes Animals',
-  //     'amount': 2
-  //   },
-  // ];
   var userCollections = [
     'https://www.familyeducation.com/sites/default/files/fe_slideshow/2008_03/Chipmunk_H.jpg',
     'https://media1.popsugar-assets.com/files/thumbor/PZXP_YZYLIecUhZhKVpH0ThoAsM/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2019/07/18/646/n/1922243/3a21fb761112a072_pet/i/Royal-Pet-Portraits.jpg',
@@ -43,74 +29,153 @@ class _collectionsScreenState extends State<collectionsScreen> {
     'https://ae01.alicdn.com/kf/H35ab780c7bd04d81a9dae76bb729b9813/Vintage-Body-Deer-Cat-Dog-Portrait.jpg_640x640.jpg'
   ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      child: MasonryGridView.count(
-        shrinkWrap: true,
-        crossAxisCount: 2,
-        itemCount: userCollections.length,
-        mainAxisSpacing: 12,
-        crossAxisSpacing: 12,
-        // shrinkWrap: true,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.only(top: 8),
-            child: InkWell(
-              child: getCollectionGroup(),
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return collectionScreen();
-                }));
-              },
-            ),
-          );
-        },
-      ),
-    );
+  Future<PostModel?> getPostJson(postId) async {
+    // log('postId: ${postId}');
+    final res = await PostServices().getPostById(postId);
+    // log('data: ${res!.imageUrl} ');
+    return res;
   }
 
-  Widget getCollectionGroup() {
+  @override
+  Widget build(BuildContext context) {
+    return NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            SliverList(delegate: SliverChildListDelegate([Container()]))
+          ];
+        },
+        body: StreamBuilder<List<CollectionModel>>(
+            stream: CollectionServices().getCollectionsOfCurrentUser(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return EmptyPostsTypeError(error: snapshot.error.toString());
+              } else if (snapshot.hasData) {
+                if (snapshot.data!.isEmpty) {
+                  return EmptyCollection();
+                }
+                final collections = snapshot.data!;
+                return Container(
+                  padding: EdgeInsets.all(8),
+                  child: MasonryGridView.count(
+                    shrinkWrap: true,
+                    crossAxisCount: 2,
+                    itemCount: collections.length,
+                    mainAxisSpacing: 12,
+                    crossAxisSpacing: 12,
+                    // shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        margin: EdgeInsets.all(2),
+                        child: InkWell(
+                          radius: 8,
+                          // child: Container(),
+                          child: getCollectionGroup(collections, index),
+                          onTap: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              // log('${collections[index].postsId}');
+                              return collectionScreen(
+                                postsId: collections[index].postsId,
+                              );
+                            }));
+                          },
+                        ),
+                        clipBehavior: Clip.antiAlias,
+                      );
+                    },
+                  ),
+                );
+              }
+              return Center(
+                  child: SpinKitDancingSquare(
+                color: AppTheme.colors.primary,
+              ));
+            }));
+  }
+
+  Widget getCollectionGroup(List<CollectionModel> collections, int index) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Stack(
-            children: userCollections
-                .getRange(0, 3)
-                .toList()
-                .asMap()
-                .entries
-                .map((item) => Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(14),
-                        border: Border.all(
-                            style: BorderStyle.solid,
-                            color: Colors.white,
-                            width: 3),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.network(
-                          item.value,
-                          width: (MediaQuery.of(context).size.width / 2) -
-                              (item.key * 40),
-                          height: 160,
-                          fit: BoxFit.cover,
-                          alignment: Alignment.center,
+        collections[index].postsId.length > 0
+            ? Stack(
+                children: collections[index]
+                    .postsId
+                    .toList()
+                    .asMap()
+                    .entries
+                    .map((item) {
+                return FutureBuilder(
+                  future: getPostJson(item.value),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return EmptyPostsTypeError(
+                          error: snapshot.error.toString());
+                    } else if (snapshot.hasData) {
+                      final post = (snapshot.data) as PostModel;
+
+                      return Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(14),
+                          border: Border.all(
+                              style: BorderStyle.solid,
+                              color: Color.fromARGB(136, 44, 44, 44),
+                              width: 2),
                         ),
-                      ),
-                    ))
-                .toList()),
-        Text('Collection name',
-            style: TextStyle(
-                color: AppTheme.colors.primaryFontColor,
-                fontSize: 14,
-                fontWeight: FontWeight.bold)),
-        Text('4 posts',
-            style: TextStyle(
-                color: AppTheme.colors.primaryFontColor, fontSize: 12)),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Opacity(
+                            opacity: item.key + 1 ==
+                                    collections[index].postsId.length
+                                ? 1
+                                : 0.6,
+                            child: Image.network(
+                              post == null ? AppTheme.src.empty : post.imageUrl,
+                              width: (MediaQuery.of(context).size.width / 2) -
+                                  (item.key * 40),
+                              height: 160,
+                              fit: BoxFit.cover,
+                              alignment: Alignment.center,
+                            ),
+                          ),
+                        ),
+                      );
+                    } else {
+                      return Center(
+                          child: CircularProgressIndicator(
+                        strokeWidth: 2,
+                      ));
+                    }
+                  },
+                );
+              }).toList())
+            : Container(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    AppTheme.src.empty,
+                    height: 160,
+                    fit: BoxFit.cover,
+                    alignment: Alignment.center,
+                  ),
+                ),
+              ),
+        Container(
+          margin: EdgeInsets.only(left: 4),
+          child: Text(collections[index].collectionName,
+              style: TextStyle(
+                  color: AppTheme.colors.primaryFontColor,
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold)),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 4),
+          padding: EdgeInsets.only(bottom: 10),
+          child: Text("Total ${collections[index].postsId.length.toString()}",
+              style: TextStyle(
+                  color: AppTheme.colors.primaryFontColor, fontSize: 12)),
+        ),
       ],
     );
   }
